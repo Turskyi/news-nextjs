bd3793015da5bfaa516b8414a525891218e49d86
Dmytro Turskyi <dmytro.turskyi@gmail.com>
2025-04-21 20:18:38 -0400

    ✨ Update contact page with Telegram support link and modify website URL

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -14,20 +14,20 @@ const ContactPage = () => {
           <p className="mb-4">
             <span className="font-bold">Phone:</span> +1 (437) 985-2581
           </p>
-          <p className="mb-4">
-            <span className="text-lg font-bold">Address:</span>
-            <address className="mt-2 block not-italic leading-relaxed">
+          <div className="mb-4">
+            <p className="text-lg font-bold mb-1">Address:</p>
+            <address className="not-italic leading-relaxed">
               3035 Finch West Avenue, <br />
               North York, Ontario, <br />
               M9M 0A3, Canada
             </address>
-          </p>
+          </div>
 
           {/* Create a p element for the website */}
           <p className="mb-4">
             <span className="font-bold">Website:</span>{' '}
-            <a href="https://turskyi.com" className="text-blue-500">
-              turskyi.com
+            <a href="https://turskyi.com/#/support" className="text-blue-500">
+              turskyi.com/support
             </a>
           </p>
           {/* Create a p element for the email */}
@@ -35,6 +35,12 @@ const ContactPage = () => {
             <span className="font-bold">Email:</span>{' '}
             <a href="mailto:dmytro@turskyi.com" className="text-blue-500">
               dmytro@turskyi.com
+            </a>
+          </p>
+          <p className="mb-4">
+            <span className="font-bold">Telegram Support:</span>{' '}
+            <a href="https://t.me/+HrCX6YD2-X82MzIy" className="text-blue-500 underline">
+              Join our support channel
             </a>
           </p>
         </div>
