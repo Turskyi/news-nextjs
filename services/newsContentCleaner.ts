c6287331636aed507c128527754eea3188b0bdb3
Dmytro Turskyi <dmytro.turskyi@gmail.com>
2026-05-31 23:29:14 -0400

    ✨ Fix regex for news API truncation marker to handle optional '+' correctly

--- services/newsContentCleaner.ts
+++ services/newsContentCleaner.ts
@@ -1,6 +1,6 @@
 import { NewsArticle } from '@/models/NewsArticles';
 
-const NEWS_API_TRUNCATION_MARKER = /\s*\[\+\d+\s*chars\]\s*$/i;
+const NEWS_API_TRUNCATION_MARKER = /\s*\[\+?\d+\s*chars\]\s*$/i;
 const MINIMUM_READABLE_WORD_COUNT = 3;
 const MINIMUM_READABLE_LETTER_COUNT = 15;
 const MINIMUM_READABLE_LENGTH = 40;
